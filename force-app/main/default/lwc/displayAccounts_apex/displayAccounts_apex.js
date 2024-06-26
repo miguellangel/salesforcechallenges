2a743b806a65b7d2fa824cb6b14e0c3d0f41d7b1
miguellangel <arriaga.miguel123@gmail.com>
2024-06-26 09:50:06 -0500

    finish imperative apex

--- force-app/main/default/lwc/displayAccounts_apex/displayAccounts_apex.js
+++ force-app/main/default/lwc/displayAccounts_apex/displayAccounts_apex.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
+import getAccountsLikeQuery from '@salesforce/apex/AccountHelper.getAccountsLikeQuery';
 
-export default class DisplayAccounts_apex extends LightningElement {}
+export default class DisplayAccounts_apex extends LightningElement {
+
+    accounts;
+
+    handleImperativeSearch() {
+        let inputValue = this.template.querySelector('.query').value;
+
+        getAccountsLikeQuery({query: inputValue})
+            .then(result => {
+                this.accounts = result;
+            })
+            .catch(error => {
+                this.accounts = undefined;
+            });
+    }
+
+}
